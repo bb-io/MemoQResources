3b5eb4d7857fd807897556293f6d14038023c096
RiabushenkoA <ariabushenko@blackbird.io>
2025-01-30 22:36:09 +0200

    Added polling on termbase updated

---
+++ Apps.MemoQResources/Polling/Models/TermbaseUpdatedResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apps.MemoQResources.Polling.Models
+{
+    public class TermbaseUpdateResponse
+    {
+        public int AccessLevel { get; set; }
+        public string Client { get; set; } = string.Empty;
+        public string Domain { get; set; } = string.Empty;
+        public string FriendlyName { get; set; } = string.Empty;
+        public List<string> Languages { get; set; } = new();
+        public DateTime LastModified { get; set; }
+        public int NumEntries { get; set; }
+        public string Project { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string TBGuid { get; set; }
+        public string TBOwner { get; set; } = string.Empty;
+        public DateTime LastUsed { get; set; }
+        public bool IsUsedInProject { get; set; }
+    }
+}
---
+++ Apps.MemoQResources/Polling/TermbasePolling.cs
@@ -0,0 +1,58 @@
+﻿using Apps.MemoQResources.Api;
+using Apps.MemoQResources.Invocables;
+using Apps.MemoQResources.Polling.Models;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Polling;
+using Blackbird.Applications.Sdk.Common.Webhooks;
+using RestSharp;
+
+namespace Apps.MemoQResources.Polling
+{
+    public class TermbasePolling : MemoQResourcesInvocable
+    {
+        public TermbasePolling(InvocationContext invocationContext) : base(invocationContext) { }
+
+        [PollingEvent("On terms added or updated", "Triggered when new terms are added or existing terms are updated")]
+        public async Task<PollingEventResponse<TermbaseMemory, TermbaseUpdateResponse>> OnTermsAddedOrUpdated(
+            PollingEventRequest<TermbaseMemory> request,
+            [WebhookParameter] TermbaseInput input)
+        {
+            var client = new MemoQResourcesClient(InvocationContext.AuthenticationCredentialsProviders);
+            var requestUrl = $"memoqserverhttpapi/v1/tbs/{input.TbId}";
+
+            var restRequest = new RestRequest(requestUrl, Method.Get);
+            var response = await client.ExecuteAsync<TermbaseUpdateResponse>(restRequest);
+
+            if (!response.IsSuccessful || response.Data == null)
+            {
+                return new PollingEventResponse<TermbaseMemory, TermbaseUpdateResponse>
+                {
+                    Memory = request.Memory,
+                    FlyBird = false
+                };
+            }
+
+            var lastModified = response.Data.LastModified;
+            var lastPollingTime = request.Memory?.LastPollingTime ?? DateTime.MinValue;
+
+            if (lastModified > lastPollingTime)
+            {
+                return new PollingEventResponse<TermbaseMemory, TermbaseUpdateResponse>
+                {
+                    Memory = new TermbaseMemory { LastPollingTime = lastModified, Triggered = true },
+                    Result = response.Data,
+                    FlyBird = true
+                };
+            }
+
+            return new PollingEventResponse<TermbaseMemory, TermbaseUpdateResponse>
+            {
+                Memory = request.Memory,
+                FlyBird = false
+            };
+        }
+
+
+      
+    }
+}
