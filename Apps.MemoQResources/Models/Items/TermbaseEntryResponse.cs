7f39a9e4e1b21e642b89c2c0724c0842fac08f0f
RiabushenkoA <ariabushenko@blackbird.io>
2025-01-31 17:00:22 +0200

    Updated naming

--- Apps.MemoQResources/Models/Items/TermbaseEntryResponse.cs
+++ Apps.MemoQResources/Models/Items/TermbaseEntryResponse.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Blackbird.Applications.Sdk.Common;
 
 namespace Apps.MemoQResources.Models.Items
 {
@@ -15,6 +16,8 @@ namespace Apps.MemoQResources.Models.Items
         public int Id { get; set; }
         public string Client { get; set; }
         public string Domain { get; set; }
+
+        [Display("Image Caption")]
         public string ImageCaption { get; set; }
         public string Image { get; set; }
         public List<LanguageItemDto> Languages { get; set; }
@@ -27,15 +30,24 @@ namespace Apps.MemoQResources.Models.Items
         public string Language { get; set; }
         public string Definition { get; set; }
         public int Id { get; set; }
+
+        [Display("Needs moderation")]
         public bool NeedsModeration { get; set; }
+
+        [Display("Term Items")]
         public List<TermItemDto> TermItems { get; set; }
     }
     public class TermItemDto
     {
+        [Display("Case sensitivity")]
         public int CaseSense { get; set; }
         public string Example { get; set; }
         public int Id { get; set; }
+
+        [Display("Is forbidden")]
         public bool IsForbidden { get; set; }
+
+        [Display("Partial match")]
         public int PartialMatch { get; set; }
         public string Text { get; set; }
     }
